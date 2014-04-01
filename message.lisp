c4e88ac48303f29de7d287e8b0b295e8be484c34
Shinmera <shinmera@tymoon.eu>
2014-04-01 15:07:59 +0200

    Added message-visible, fixed level shorthand definition

--- message.lisp
+++ message.lisp
@@ -6,7 +6,11 @@
 
 (in-package :verbose)
 
+(defvar *levels* (list :TRACE :DEBUG :INFO :WARN :ERROR :SEVERE :FATAL)
+  "List of accepted logging level names. The position in the list defines its level, ranking from finest to roughest.")
+
 (defun log-object (object)
+  "Shorthand for (pass *global-controller* object)"
   (pass *global-controller* object))
 
 (defclass message ()
@@ -26,20 +30,27 @@
             (message-content message)))
   message)
 
+(defgeneric message-visible (message level)
+  (:documentation "Returns T if the message is visible under the given level.")
+  (:method ((message message) (level symbol))
+    (<= (position level *levels*) (position (message-level message) *levels*))))
+
 (defun log-message (level category content)
-  (assert (find level '(:FATAL :SEVERE :ERROR :WARN :INFO :DEBUG :TRACE)) (level)
+  "Logs a message with the given parameters."
+  (assert (find level *levels*) (level)
           "Level must be one of (:FATAL :SEVERE :ERROR :WARN :INFO :DEBUG :TRACE).")
   (assert (keywordp category) (category)
           "Category must be a keyword.")
   (log-object (make-instance 'message :level level :category category :content content)))
 
 (defun log (level category format-string &rest format-args)
+  "Logs a new message under given level and category, using the format-string and arguments as message text."
   (log-message level category (apply #'format NIL format-string format-args)))
 
-(defmacro define-levels ()
-  `(progn
-     ,@(loop for level in '(FATAL SEVERE ERROR WARN INFO DEBUG TRACE)
-          collect `(defmacro ,level (category format-string &rest format-args)
-                     `(log ,,(intern (symbol-name level) "KEYWORD") ,category ,format-string ,@format-args)))))
+(defmacro define-level (level)
+  "Define a new shorthand function for logging under a specific level."
+  `(defun ,(intern (symbol-name level) "VERBOSE") (category format-string &rest format-args)
+     (apply #'log ,(intern (symbol-name level) "KEYWORD") category format-string format-args)))
 
-(define-levels)
+(eval `(progn ,@(loop for level in *levels*
+                      collect `(define-level ,level))))
