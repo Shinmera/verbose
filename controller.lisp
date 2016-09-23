b84f06eb301701f863bd3749019b441791125b88
Shinmera <shinmera@tymoon.eu>
2016-09-23 13:20:05 +0200

    Rename some things, swap buffers rather than creating a new one every time.

--- controller.lisp
+++ controller.lisp
@@ -12,9 +12,10 @@
 (defclass controller (pipeline)
   ((thread :initform NIL :accessor thread)
    (thread-continue :initform NIL :accessor thread-continue)
-   (message-pipe :initform (make-array '(10) :adjustable T :fill-pointer 0) :accessor message-pipe)
-   (message-condition :initform (bt:make-condition-variable :name "MESSAGE-CONDITION") :reader message-condition)
-   (message-lock :initform (bt:make-lock "MESSAGE-LOCK") :reader message-lock)))
+   (queue :initform (make-array '(10) :adjustable T :fill-pointer 0) :accessor queue)
+   (queue-back :initform (make-array '(10) :adjustable T :fill-pointer 0) :accessor queue-back)
+   (queue-condition :initform (bt:make-condition-variable :name "MESSAGE-CONDITION") :reader queue-condition)
+   (queue-lock :initform (bt:make-lock "MESSAGE-LOCK") :reader queue-lock)))
 
 (defmethod initialize-instance :after ((controller controller) &key)
   (start controller))
@@ -22,7 +23,7 @@
 (defmethod print-object ((controller controller) stream)
   (print-unreadable-object (controller stream :type T)
     (format stream "~@[:threaded~*~]~@[ :running~*~] :queue-size ~d"
-            (thread controller) (thread-continue controller) (length (message-pipe controller)))))
+            (thread controller) (thread-continue controller) (length (queue controller)))))
 
 (defmethod start ((controller controller))
   (setf (thread-continue controller) T)
@@ -43,7 +44,7 @@
   (loop for thread = (thread controller)
         for i from 0
         while (and thread (bt:thread-alive-p thread))
-        do (bt:condition-notify (message-condition controller))
+        do (bt:condition-notify (queue-condition controller))
            (sleep 0.1)
            (when (< 5 i)
              (bt:destroy-thread thread)
@@ -51,25 +52,28 @@
   controller)
 
 (defmacro with-controller-lock ((&optional (controller '*global-controller*)) &body forms)
-  `(bt:with-lock-held ((message-lock ,controller))
+  `(bt:with-lock-held ((queue-lock ,controller))
      ,@forms))
 
 (defmethod controller-loop ((controller controller))
-  (let* ((lock (message-lock controller))
-         (condition (message-condition controller))
+  (let* ((lock (queue-lock controller))
+         (condition (queue-condition controller))
          (pipeline (pipeline controller)))
     (bt:acquire-lock lock)
     (unwind-protect
          (loop do (with-simple-restart (skip "Skip processing the message batch.")
-                    (let ((queue (message-pipe controller)))
-                      (setf (message-pipe controller) (make-array '(10) :adjustable T :fill-pointer 0))
+                    (let ((queue (queue controller)))
+                      (rotatef (queue controller) (queue-back controller))
                       (bt:release-lock lock)
-                      (loop for thing across queue
+                      (loop for i from 0
+                            for thing across queue
                             do (with-simple-restart (continue "Continue processing messages, skipping ~a" thing)
-                                 (pass pipeline thing)))))
+                                 (pass pipeline thing))
+                               (setf (aref queue i) 0))
+                      (setf (fill-pointer queue) 0)))
                   (bt:acquire-lock lock)
-                  (when (= 0 (length (message-pipe controller)))
-                    (bt:condition-wait condition lock :timeout 1))
+                  (when (= 0 (length (queue controller)))
+                    (bt:condition-wait condition lock :timeout 0.1))
                while (thread-continue controller))
       (setf (thread controller) NIL)
       (ignore-errors (bt:release-lock lock)))))
@@ -80,8 +84,8 @@
                 thread (bt:thread-alive-p thread)
                 (not (eql (bt:current-thread) thread)))
            (with-controller-lock (controller)
-             (vector-push-extend message (message-pipe controller)))
-           (bt:condition-notify (message-condition controller)))
+             (vector-push-extend message (queue controller)))
+           (bt:condition-notify (queue-condition controller)))
           (T
            (pass (pipeline controller) message))))
   NIL)
